fe48814482600892197e9c44690429caf259c3b6
Ramela M S <ramelams07@gmail.com>
2026-04-04 12:35:52 +0530

    remove cache files

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from .routes import init_routes
 from .extensions import db,login_manager
 from .forms import LoginForm, RegisterForm
-
+import os
 app = Flask(__name__)
 init_routes(app)
 
 app.config["SECRET_KEY"] = "ramelamutharasususdha"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///app.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
